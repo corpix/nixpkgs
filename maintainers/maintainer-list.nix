900486d56e9ed2499dc2261f00a6b55013fa6fd8
Ding Xiang Fei <dingxiangfei2009@gmail.com>
2019-05-27 15:56:29 +0800

    fix maintainer list

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -1377,11 +1377,6 @@
     github = "dingxiangfei2009";
     name = "Ding Xiang Fei";
   };
-  dxf = {
-    email = "dingxiangfei2009@gmail.com";
-    github = "dingxiangfei2009";
-    name = "Ding Xiang Fei";
-  };
   dysinger = {
     email = "tim@dysinger.net";
     github = "dysinger";
