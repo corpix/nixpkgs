1cb627f558f34f91c39f05de496ed65200388f19
Dmitry Moskowski <me@corpix.ru>
2019-07-23 13:02:41 +0000

    udiskie: add wrapQtAppsHook
    This commit fixes the error:
    
    Could not find the Qt platform plugin "xcb" in ""

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -21389,7 +21389,7 @@ in
 
   udevil = callPackage ../applications/misc/udevil {};
 
-  udiskie = callPackage ../applications/misc/udiskie { };
+  udiskie = libsForQt5.callPackage ../applications/misc/udiskie { };
 
   sakura = callPackage ../applications/misc/sakura { };
 
