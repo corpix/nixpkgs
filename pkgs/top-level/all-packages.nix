5f5d90e21ea5af9ab02dd4072d542211721fe089
Dmitry Moskowski <me@corpix.ru>
2019-07-28 05:38:55 +0000

    concourse: adding from https://github.com/tenx-tech/nixpkgs/tree/concourse-5.2/nixos/modules/services/continuous-integration/concourse

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -165,6 +165,10 @@ in
 
   corgi = callPackage ../development/tools/corgi { };
 
+  inherit (callPackage ../development/tools/continuous-integration/concourse { }) concourse fly;
+
+  garden-runc = callPackage ../development/tools/continuous-integration/concourse/garden-runc { };
+
   dhallToNix = callPackage ../build-support/dhall-to-nix.nix {
     inherit dhall-nix;
   };
@@ -10024,8 +10028,6 @@ in
     inherit (darwin.apple_sdk.frameworks) CoreServices;
   };
 
-  fly = callPackage ../development/tools/continuous-integration/fly { };
-
   foreman = callPackage ../tools/system/foreman { };
   goreman = callPackage ../tools/system/goreman { };
 
@@ -16779,6 +16781,8 @@ in
   go-bindata = callPackage ../development/tools/go-bindata { };
 
   go-bindata-assetfs = callPackage ../development/tools/go-bindata-assetfs { };
+
+  go-packr = callPackage ../development/tools/go-packr { };
 
   go-protobuf = callPackage ../development/tools/go-protobuf { };
 
