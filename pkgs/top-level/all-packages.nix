a2f883ec90419890628149886db3263c8dce4af4
Dmitry Moskowski <me@corpix.ru>
2020-03-24 17:29:05 +0000

    virtscreen: fixed start up

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -22891,7 +22891,7 @@ in
 
   virtinst = callPackage ../applications/virtualization/virtinst {};
 
-  virtscreen = callPackage ../tools/admin/virtscreen {};
+  virtscreen = libsForQt5.callPackage ../tools/admin/virtscreen {};
 
   virtualbox = libsForQt5.callPackage ../applications/virtualization/virtualbox {
     stdenv = stdenv_32bit;
