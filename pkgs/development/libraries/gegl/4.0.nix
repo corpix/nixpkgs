f5e59c948f7c92103dee9969f0363dd34d681a7e
Dmitry Moskowski <me@corpix.ru>
2020-04-13 22:49:54 +0000

    Revert "gegl: fixed the build"
    This reverts commit ac386102a081eb158c5693f51daec2435ba39243.

--- pkgs/development/libraries/gegl/4.0.nix
+++ pkgs/development/libraries/gegl/4.0.nix
@@ -37,7 +37,7 @@ stdenv.mkDerivation rec {
   pname = "gegl";
   version = "0.4.22";
 
-  outputs = [ "out" "dev" ];
+  outputs = [ "out" "dev" "devdoc" ];
   outputBin = "dev";
 
   src = fetchurl {
