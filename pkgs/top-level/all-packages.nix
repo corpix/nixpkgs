0e16c8a37f56d20545d700c99c736b2911f8ca48
Dmitry Moskowski <me@corpix.ru>
2020-05-24 22:39:02 +0000

    kaidan: init

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8028,6 +8028,8 @@ in
 
   zxing = callPackage ../tools/graphics/zxing {};
 
+  zxing-cpp = callPackage ../tools/graphics/zxing-cpp {};
+
   zmap = callPackage ../tools/security/zmap { };
 
 
@@ -14566,6 +14568,8 @@ in
 
     qwt = callPackage ../development/libraries/qwt/6.nix { };
 
+    qxmpp = callPackage ../development/libraries/qxmpp { };
+
     telepathy = callPackage ../development/libraries/telepathy/qt { };
 
     vlc = callPackage ../applications/video/vlc {};
@@ -20692,6 +20696,8 @@ in
   okteta = libsForQt5.callPackage ../applications/editors/okteta { };
 
   k4dirstat = libsForQt5.callPackage ../applications/misc/k4dirstat { };
+
+  kaidan = libsForQt5.callPackage ../applications/networking/instant-messengers/kaidan { };
 
   kdeconnect = libsForQt5.callPackage ../applications/misc/kdeconnect { };
 
