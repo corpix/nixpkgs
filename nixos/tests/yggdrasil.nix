3c4c9b5eb4156fabac0988be40f7d1e5eefd5c2a
Dmitry Moskowski <me@corpix.ru>
2020-05-25 01:51:07 +0000

    Revert "nixos/yggdrasil: change config priority, persistentKeys"
    This reverts commit ac97b19a2ab6901af26cee6878ffa73de68d1fcb.

--- nixos/tests/yggdrasil.nix
+++ nixos/tests/yggdrasil.nix
@@ -122,7 +122,6 @@ in import ./make-test-python.nix ({ pkgs, ...} : {
             MulticastInterfaces = [ "eth1" ];
             LinkLocalTCPPort = 43210;
           };
-          persistentKeys = true;
         };
       };
     };
