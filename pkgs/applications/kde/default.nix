d23e5adda874df1061cc61d972fe5e89a23ce61a
Dmitry Moskowski <me@corpix.ru>
2020-09-04 12:39:23 +0000

    kdav: remove

--- pkgs/applications/kde/default.nix
+++ pkgs/applications/kde/default.nix
@@ -106,7 +106,6 @@ let
       kcalutils = callPackage ./kcalutils.nix {};
       kcharselect = callPackage ./kcharselect.nix {};
       kcolorchooser = callPackage ./kcolorchooser.nix {};
-      kdav = callPackage ./kdav.nix {};
       kbackup = callPackage ./kbackup.nix {};
       kdebugsettings = callPackage ./kdebugsettings.nix {};
       kdegraphics-mobipocket = callPackage ./kdegraphics-mobipocket.nix {};
