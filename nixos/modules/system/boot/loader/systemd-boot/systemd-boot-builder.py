4616e53892e2a48bf8c7c6ae10f29aba3270cdc8
Tethys Svensson <freaken@freaken.dk>
2020-09-10 20:10:30 +0200

    nixos/systemd-boot: Temporarily ignore errors
    This is a temporary fix for #97433. A more proper fix has been
    implemented upstream in systemd/systemd#17001, however until it gets
    backported, we are stuck with ignoring the error.
    
    After the backport lands, this commit should be reverted.

--- nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
+++ nixos/modules/system/boot/loader/systemd-boot/systemd-boot-builder.py
@@ -200,7 +200,7 @@ def main():
     else:
         # Update bootloader to latest if needed
         systemd_version = subprocess.check_output(["@systemd@/bin/bootctl", "--version"], universal_newlines=True).split()[1]
-        sdboot_status = subprocess.check_output(["@systemd@/bin/bootctl", "--path=@efiSysMountPoint@", "status"], universal_newlines=True)
+        sdboot_status = subprocess.run(["@systemd@/bin/bootctl", "--path=@efiSysMountPoint@", "status"], universal_newlines=True, stdout=subprocess.PIPE).stdout
 
         # See status_binaries() in systemd bootctl.c for code which generates this
         m = re.search("^\W+File:.*/EFI/(BOOT|systemd)/.*\.efi \(systemd-boot (\d+)\)$",
