114dd3293707b08377fbb2ddcd2000ad85b03edd
Dmitry Moskowski <me@corpix.ru>
2021-12-15 20:55:16 +0000

    make zfs enable non readonly

--- nixos/modules/tasks/filesystems/zfs.nix
+++ nixos/modules/tasks/filesystems/zfs.nix
@@ -212,7 +212,7 @@ in
       };
 
       enabled = mkOption {
-        readOnly = true;
+        #readOnly = true;
         type = types.bool;
         default = inInitrd || inSystem;
         defaultText = literalMD "`true` if ZFS filesystem support is enabled";
