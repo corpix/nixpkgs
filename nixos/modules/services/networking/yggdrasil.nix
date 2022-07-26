4915f20bf5007b63645e56aa06f14738b7babcac
Dmitry Moskowski <me@corpix.ru>
2022-07-26 00:48:50 +0000

    ygddrasil: restore my version after rebase

--- nixos/modules/services/networking/yggdrasil.nix
+++ nixos/modules/services/networking/yggdrasil.nix
@@ -1,15 +1,13 @@
 { config, lib, pkgs, ... }:
 with lib;
 let
-  keysPath = "/var/lib/yggdrasil/keys.json";
-
   cfg = config.services.yggdrasil;
   settingsProvided = cfg.settings != { };
   configFileProvided = cfg.configFile != null;
 
   format = pkgs.formats.json { };
-in
-{
+  keysPath = "${cfg.dataDir}/keys.json";
+in {
   imports = [
     (mkRenamedOptionModule
       [ "services" "yggdrasil" "config" ]
@@ -19,6 +17,12 @@ in
   options = with types; {
     services.yggdrasil = {
       enable = mkEnableOption (lib.mdDoc "the yggdrasil system service");
+
+      dataDir = mkOption {
+        description = lib.mdDoc "Yggdrasil data directory";
+        type = path;
+        default = "/var/lib/yggdrasil";
+      };
 
       settings = mkOption {
         type = format.type;
@@ -72,11 +76,18 @@ in
         '';
       };
 
-      group = mkOption {
+      user = mkOption {
         type = types.nullOr types.str;
-        default = null;
+        default = "yggdrasil";
+        example = "ygg";
+        description = lib.mdDoc "User to run yggdrasil service from.";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "yggdrasil";
         example = "wheel";
-        description = lib.mdDoc "Group to grant access to the Yggdrasil control socket. If `null`, only root can access the socket.";
+        description = lib.mdDoc "Group to grant access to the Yggdrasil control socket.";
       };
 
       openMulticastPort = mkOption {
@@ -118,7 +129,7 @@ in
       persistentKeys = mkEnableOption (lib.mdDoc ''
         persistent keys. If enabled then keys will be generated once and Yggdrasil
         will retain the same IPv6 address when the service is
-        restarted. Keys are stored at ${keysPath}
+        restarted. Keys are stored at ${keysPath}.
       '');
 
       extraArgs = mkOption {
@@ -142,54 +153,49 @@ in
         message = "networking.enableIPv6 must be true for yggdrasil to work";
       }];
 
-      system.activationScripts.yggdrasil = mkIf cfg.persistentKeys ''
-        if [ ! -e ${keysPath} ]
-        then
-          mkdir --mode=700 -p ${builtins.dirOf keysPath}
-          ${binYggdrasil} -genconf -json \
-            | ${pkgs.jq}/bin/jq \
-                'to_entries|map(select(.key|endswith("Key")))|from_entries' \
-            > ${keysPath}
-        fi
-      '';
+    system.activationScripts.yggdrasil = mkIf cfg.persistentKeys ''
+      if [ ! -e ${keysPath} ]
+      then
+        mkdir --mode=700 -p ${builtins.dirOf keysPath}
+        ${binYggdrasil} -genconf -json \
+          | ${pkgs.jq}/bin/jq \
+              'to_entries|map(select(.key|endswith("Key")))|from_entries' \
+          > ${keysPath}
+        chown -R yggdrasil ${builtins.dirOf keysPath}
+      fi
+    '';
 
-      systemd.services.yggdrasil = {
-        description = "Yggdrasil Network Service";
-        after = [ "network-pre.target" ];
-        wants = [ "network.target" ];
-        before = [ "network.target" ];
-        wantedBy = [ "multi-user.target" ];
+    systemd.services.yggdrasil = {
+      description = "Yggdrasil Network Service";
+      after = [ "network-pre.target" ];
+      wants = [ "network.target" ];
+      before = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
 
-        # This script first prepares the config file, then it starts Yggdrasil.
-        # The preparation could also be done in ExecStartPre/preStart but only
-        # systemd versions >= v252 support reading credentials in ExecStartPre. As
-        # of February 2023, systemd v252 is not yet in the stable branch of NixOS.
-        #
-        # This could be changed in the future once systemd version v252 has
-        # reached NixOS but it does not have to be. Config file preparation is
-        # fast enough, it does not need elevated privileges, and `set -euo
-        # pipefail` should make sure that the service is not started if the
-        # preparation fails. Therefore, it is not necessary to move the
-        # preparation to ExecStartPre.
-        script = ''
-          set -euo pipefail
+      preStart =
+        (if settingsProvided || configFileProvided || cfg.persistentKeys
+         then concatStringsSep "\n" [
+           "set -o pipefail"
+           "{"
+           "echo ${optionalString settingsProvided "'${builtins.toJSON cfg.settings}'"}"
+           (optionalString configFileProvided "cat ${cfg.configFile}")
+           (optionalString cfg.persistentKeys "cat ${keysPath}")
+           "} | ${pkgs.jq}/bin/jq -s add | ${binYggdrasil} -normaliseconf -useconf"
+         ]
+         else "${binYggdrasil} -genconf") + " > /run/yggdrasil/yggdrasil.conf";
 
-          # prepare config file
-          ${(if settingsProvided || configFileProvided || cfg.persistentKeys then
-            "echo "
+      serviceConfig = {
+        User = cfg.user;
+        Group = cfg.group;
+        StateDirectory = "yggdrasil";
+        RuntimeDirectory = "yggdrasil";
+        RuntimeDirectoryMode = "0750";
+        ReadOnlyPaths = lib.optional configFileProvided cfg.configFile ++ lib.optional cfg.persistentKeys keysPath;
+        ReadWritePaths = "/run/yggdrasil";
 
-            + (lib.optionalString settingsProvided
-              "'${builtins.toJSON cfg.settings}'")
-            + (lib.optionalString configFileProvided
-              "$(${binHjson} -c \"$CREDENTIALS_DIRECTORY/yggdrasil.conf\")")
-            + (lib.optionalString cfg.persistentKeys "$(cat ${keysPath})")
-            + " | ${pkgs.jq}/bin/jq -s add | ${binYggdrasil} -normaliseconf -useconf"
-          else
-            "${binYggdrasil} -genconf") + " > /run/yggdrasil/yggdrasil.conf"}
-
-          # start yggdrasil
-          ${binYggdrasil} -useconffile /run/yggdrasil/yggdrasil.conf ${lib.strings.escapeShellArgs cfg.extraArgs}
-        '';
+        ExecStart = "${binYggdrasil} -useconffile /run/yggdrasil/yggdrasil.conf ${lib.strings.escapeShellArgs cfg.extraArgs}";
+        ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
+        Restart = "always";
 
         serviceConfig = {
           ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
@@ -219,9 +225,19 @@ in
           Group = cfg.group;
         } else { });
       };
+    };
 
       networking.dhcpcd.denyInterfaces = cfg.denyDhcpcdInterfaces;
       networking.firewall.allowedUDPPorts = mkIf cfg.openMulticastPort [ 9001 ];
+
+      users.groups.${cfg.group} = {};
+      users.users.${cfg.user} = {
+        description = "Yggdrasil daemon user";
+        home = cfg.dataDir;
+        createHome = true;
+        isSystemUser = true;
+        group = cfg.group;
+      };
 
       # Make yggdrasilctl available on the command line.
       environment.systemPackages = [ cfg.package ];
