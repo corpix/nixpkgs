5e9d4a26f63143ea7018d8e8042bf7582bd94495
Dmitry Moskowski <me@corpix.ru>
2022-12-24 21:47:26 +0000

    gambit: 9.4.3 -> 9.4.4

--- pkgs/development/compilers/gambit/gambit-support.nix
+++ pkgs/development/compilers/gambit/gambit-support.nix
@@ -1,11 +1,8 @@
-{ pkgs, lib }:
-
-rec {
+{ pkgs, lib }: rec {
   stable-params = {
     stable = true;
     defaultRuntimeOptions = "f8,-8,t8";
     buildRuntimeOptions = "f8,-8,t8";
-    fix-stamp = git-version : "";
     targets = "java,js,php,python,ruby";
     modules = false;
   };
@@ -14,18 +11,11 @@ rec {
     stable = false;
     defaultRuntimeOptions = "iL,fL,-L,tL";
     buildRuntimeOptions = "i8,f8,-8,t8";
-    fix-stamp = git-version : ''
-      substituteInPlace configure \
-        --replace "$(grep '^PACKAGE_VERSION=.*$' configure)" 'PACKAGE_VERSION="v${git-version}"' \
-        --replace "$(grep '^PACKAGE_STRING=.*$' configure)" 'PACKAGE_STRING="Gambit v${git-version}"' ;
-    '';
     targets = "arm,java,js,php,python,riscv-32,riscv-64,ruby,x86,x86-64"; # eats 100% cpu on _digest
     modules = false;
   };
 
-  export-gambopt = params : "export GAMBOPT=${params.buildRuntimeOptions} ;";
-
-  gambit-bootstrap = import ./bootstrap.nix ( pkgs );
+  export-gambopt = params: "export GAMBOPT=${params.buildRuntimeOptions} ;";
 
   meta = {
     description = "Optimizing Scheme to C compiler";
