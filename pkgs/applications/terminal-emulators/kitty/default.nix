14bcc80651872a40154a4113a86f28ae242b121f
Dmitry Moskowski <me@corpix.ru>
2023-01-09 10:27:58 +0000

    kitty: silence unsupported screen mode errors

--- pkgs/applications/terminal-emulators/kitty/default.nix
+++ pkgs/applications/terminal-emulators/kitty/default.nix
@@ -98,6 +98,9 @@ buildPythonApplication rec {
     # Skip `test_ssh_bootstrap_with_different_launchers` when launcher is `zsh` since it causes:
     # OSError: master_fd is in error condition
     ./disable-test_ssh_bootstrap_with_different_launchers.patch
+
+    # silence "Unsupported screen mode" flood
+    ./silence.patch
   ];
 
   hardeningDisable = [
