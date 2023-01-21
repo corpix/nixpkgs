a1b8890b0d6661e55c7cb810f391ff646ca67e02
Dmitry Moskowski <me@corpix.ru>
2023-01-21 01:09:13 +0000

    prometheus: autogenerated wireguard peers for exporter friendly_names
    depends on: 714c807bdfc

--- nixos/modules/services/monitoring/prometheus/exporters/wireguard.nix
+++ nixos/modules/services/monitoring/prometheus/exporters/wireguard.nix
@@ -15,7 +15,14 @@ in {
 
     wireguardConfig = mkOption {
       type = with types; nullOr (either path str);
-      default = null;
+      default = pkgs.writeText "wg.conf"
+        (concatMapStringsSep "\n" (peer: ''
+          [Peer]
+          # friendly_name = ${peer.name}
+          PublicKey = ${peer.publicKey}
+          AllowedIPs = ${concatStringsSep "," peer.allowedIPs}
+        '') (flatten (map (interface: interface.peers)
+          (attrValues config.networking.wireguard.interfaces))));
 
       description = lib.mdDoc ''
         Path to the Wireguard Config to
