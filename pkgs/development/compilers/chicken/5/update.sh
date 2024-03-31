1741af81de9910439304a23cf4a7f6a3573d504e
Dmitry Moskowski <me@corpix.ru>
2024-03-31 21:41:55 +0000

    chicken: make update.sh work with bash

--- pkgs/development/compilers/chicken/5/update.sh
+++ pkgs/development/compilers/chicken/5/update.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -I nixpkgs=../../../../.. -i oil -p oil chicken
+#! nix-shell -I nixpkgs=../../../../.. -p chicken -i bash
+set -e
 
 export URL_PREFIX="https://code.call-cc.org/egg-tarballs/5/"
+chicken_nixpkgs=$(pwd)
 cd $(nix-prefetch-url \
      'https://code.call-cc.org/cgi-bin/gitweb.cgi?p=eggs-5-latest.git;a=snapshot;h=master;sf=tgz' \
      --name chicken-eggs-5-latest --unpack --print-path | tail -1)
 
-echo "# THIS IS A GENERATED FILE.  DO NOT EDIT!" > $_this_dir/deps.toml
-for i, item in */*/*.egg {
-  var EGG_NAME=$(dirname $(dirname $item))
-  var EGG_VERSION=$(basename $(dirname $item))
-  var EGG_URL="${URL_PREFIX}${EGG_NAME}/${EGG_NAME}-${EGG_VERSION}.tar.gz"
-  var EGG_SHA256=$(nix-prefetch-url $EGG_URL)
-  export EGG_NAME
-  export EGG_VERSION
-  export EGG_SHA256
-  csi -s $_this_dir/read-egg.scm < $item
-} >> $_this_dir/deps.toml
+echo "# THIS IS A GENERATED FILE.  DO NOT EDIT!" > $chicken_nixpkgs/deps.toml
+for item in */*/*.egg
+do
+  export EGG_NAME=$(dirname $(dirname $item))
+  export EGG_VERSION=$(basename $(dirname $item))
+  export EGG_URL="${URL_PREFIX}${EGG_NAME}/${EGG_NAME}-${EGG_VERSION}.tar.gz"
+  export EGG_SHA256=$(nix-prefetch-url $EGG_URL)
+  csi -s $chicken_nixpkgs/read-egg.scm < $item
+done >> $chicken_nixpkgs/deps.toml
