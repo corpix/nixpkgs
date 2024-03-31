7bb46e149ce6ea6b9edf46c0de447b78732f35bc
Dmitry Moskowski <me@corpix.ru>
2024-03-31 22:31:49 +0000

    chicken: fix comparse

--- pkgs/development/compilers/chicken/5/overrides.nix
+++ pkgs/development/compilers/chicken/5/overrides.nix
@@ -177,6 +177,12 @@ in
         --replace "-lrt" ""
     '';
   };
+  comparse = old: {
+    postPatch = ''
+      substituteInPlace comparse.egg \
+        --replace '(lazy-seq "0.1.0")' 'lazy-seq'
+    '';
+  };
 
   # platform changes
   pledge = addMetaAttrs { platforms = lib.platforms.openbsd; };
