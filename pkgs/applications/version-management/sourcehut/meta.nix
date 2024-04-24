0bde0c7ea469b70200ccc872f5915a60415bb55a
Dmitry Moskowski <me@corpix.ru>
2024-04-24 01:27:39 +0000

    sourcehut: add patch for failing qrcode generator

--- pkgs/applications/version-management/sourcehut/meta.nix
+++ pkgs/applications/version-management/sourcehut/meta.nix
@@ -42,6 +42,7 @@ buildPythonPackage rec {
 
   disabled = pythonOlder "3.7";
 
+  patches = [./patches/meta.qrcode.patch];
   postPatch = ''
     substituteInPlace Makefile \
       --replace "all: api" ""
